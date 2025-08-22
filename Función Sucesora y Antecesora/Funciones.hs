cf16510fc05efdb4ad670ce9084173d8a38034ca
SebasCPM50 <sebastianchaux31@gmail.com>
2025-08-22 07:46:02 -0500

    Funciones.hs

--- Función Sucesora y Antecesora/Funciones.hs
+++ Función Sucesora y Antecesora/Funciones.hs
@@ -1,24 +1,23 @@
--- sucesor: devuelve el número que sigue
+--- Sucesor y predecesor
 sucesor :: Int -> Int
 sucesor n = n + 1
 
--- antecesor: devuelve el número anterior
-antecesor :: Int -> Int
-antecesor n = n - 1
+predecesor :: Int -> Int
+predecesor n = n - 1
 
--- cualquier número que se sume con 0 da el mismo número
+-- Suma y multiplicación de enteros positivos
 suma :: Int -> Int -> Int
 suma a 0 = a
-suma a b = suma (sucesor a) (antecesor b)
-
--- cualquier número que se reste entre 0 da el mismo número
-resta :: Int -> Int -> Int
-resta a 0 = a                    
-resta a b = resta (antecesor a) (antecesor b)
+suma a b = suma (sucesor a) (predecesor b)
 
 multiplicacion :: Int -> Int -> Int
 multiplicacion _ 0 = 0
-multiplicacion a b = suma a (multiplicacion a (antecesor b))
+multiplicacion a b = suma a (multiplicacion a (predecesor b))
+
+-- Resta y división de enteros positivos
+resta :: Int -> Int -> Int
+resta a 0 = a
+resta a b = resta (predecesor a) (predecesor b)
 
 division :: Int -> Int -> Int
 division _ 0 = error "No se puede dividir entre 0"
@@ -26,9 +25,15 @@ division a b
     | a < b     = 0
     | otherwise = sucesor (division (resta a b) b)
 
+-- Suma de números reales (Float)
+addReal :: Float -> Float -> Float
+addReal x y = x + y
+
+-- Función main para los ejemplos
 main :: IO ()
 main = do
     print (suma 5 3)
-    print (resta 5 3)
     print (multiplicacion 5 3)
+    print (resta 5 3)
     print (division 10 2)
+    print (addReal 3.2 4.5)
